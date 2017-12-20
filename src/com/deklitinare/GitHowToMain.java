eb576f81988adf160e5eb0083f23eaa189c4abd6
HoKipsy <remenyireka2000@gmail.com>
2017-12-20 14:48:44 +0100

    csak a harommal oszthato

--- src/com/deklitinare/GitHowToMain.java
+++ src/com/deklitinare/GitHowToMain.java
@@ -5,9 +5,9 @@ public class GitHowToMain {
 	public static void main(String[] args) {
 
 		for (int i = 1; i <= 1000; i++) {
-			System.out.println(i);
+			if(i%3 ==0) {
+				System.out.println(i);
+			}
 		}
-
 	}
-
 }
