ceb43ab89064f7828b81fe3ebbd8410806d6bb51
HoKipsy <remenyireka2000@gmail.com>
2017-12-20 14:54:03 +0100

    Szépen, szóközökkel és hármasával számolva

--- src/com/deklitinare/GitHowToMain.java
+++ src/com/deklitinare/GitHowToMain.java
@@ -5,7 +5,7 @@ public class GitHowToMain {
 	public static void main(String[] args) {
 
 		for (int i = 1; i <= 1000; i++) {
-			if(i%3 ==0) {
+			if(i%3 == 0) {
 				System.out.println(i);
 			}
 		}
