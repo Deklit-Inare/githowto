aa7842610192126170faf438b70fe7b72d8da28d
Ágoston Németh <nemagost@gmail.com>
2017-12-20 14:17:54 +0100

    Added the number printer program

---
+++ src/com/deklitinare/GitHowToMain.java
@@ -0,0 +1,13 @@
+package com.deklitinare;
+
+public class GitHowToMain {
+
+	public static void main(String[] args) {
+
+		for (int i = 1; i <= 1000; i++) {
+			System.out.println(i);
+		}
+
+	}
+
+}
